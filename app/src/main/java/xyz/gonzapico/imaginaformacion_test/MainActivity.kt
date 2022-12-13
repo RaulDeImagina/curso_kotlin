bb1394954965ae1880461547be758e402aeda5c8
Aperez <albertoperez@codisys.es>
2022-12-13 17:18:54 +0100

    Ejercicios 1, 2, 3 - Tema 13

--- app/src/main/java/xyz/gonzapico/imaginaformacion_test/MainActivity.kt
+++ app/src/main/java/xyz/gonzapico/imaginaformacion_test/MainActivity.kt
@@ -6,33 +6,29 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.gonzapico.imaginaformacion_test.databinding.ActivityMainBinding
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
+import org.koin.core.scope.Scope
 
 class MainActivity : AppCompatActivity(), UserView {
 
     private lateinit var binding: ActivityMainBinding
     private val userListViewModel: UsersViewModel by viewModels()
 
+    private val userPresenter: UserPresenter by inject {
+        parametersOf(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-
-        setUpViewModel()
-        getUsers()
-
     }
 
-    private fun getUsers() {
-        showLoading()
-        userListViewModel.getUsers()
-        hideLoading()
-    }
-
-    private fun setUpViewModel() {
-        userListViewModel.usersModel.observe(this, { userList ->
-            loadUsers(userList)
-        })
+    override fun onResume() {
+        super.onResume()
+        userPresenter.getUsers()
     }
 
     override fun showLoading() {
